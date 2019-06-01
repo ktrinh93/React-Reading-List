a8862e3549e2b9a2a4d163dc1a97cd9bd46d7ea3
ktrinh93 <ktrinh93@gmail.com>
2019-06-01 13:43:31 -0400

    init commit for react reading list app

---
+++ client/build/precache-manifest.32591d71020ad38a358835ed4eedb87b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c108bde9d321904ff089",
+    "url": "/static/js/main.6623d0fb.chunk.js"
+  },
+  {
+    "revision": "73a92ea81af2477f9057",
+    "url": "/static/js/2.44b3b530.chunk.js"
+  },
+  {
+    "revision": "c108bde9d321904ff089",
+    "url": "/static/css/main.508dabb7.chunk.css"
+  },
+  {
+    "revision": "b4d8f094c881751de78c2fce5a6e1abe",
+    "url": "/index.html"
+  }
+];
